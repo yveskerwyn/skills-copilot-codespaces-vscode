06f6c71d6378479ebbca29b14f25cb21f7b689fd
Yves Kerwyn <yves@vreegoebezig.be>
2023-08-07 15:14:50 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsmember() {
+    var skills = ['html', 'css', 'js', 'php', 'mysql'];
+    var member = {
+        name: 'John',
+        age: 20,
+        skills: skills,
+        address: {
+            city: 'Ha Noi',
+            country: 'Viet Nam'
+        }
+    };
+    console.log(member.skills[1]);
+    console.log(member.address.city);
+}
